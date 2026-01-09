a03198218d4498d4e242671d202ee6f955c5e745
Taehee Kim <rabolution@gmail.com>
2026-01-09 21:28:11 +0900

    feat: isBalanceZero predicate

--- week2/transaction/domain.ts
+++ week2/transaction/domain.ts
@@ -1,5 +1,13 @@
-import type { SimpleTransaction } from './type';
+import type { SimpleTransaction } from "./type";
+
+function sum(arr: Iterable<number>): number {
+  let total = 0;
+  for (const num of arr) {
+    total += num;
+  }
+  return total;
+}
 
 export function isBalanceZero(transaction: SimpleTransaction): boolean {
-    return false // ??? 
+  return sum(transaction.postings.map((p) => p.amount)) === 0;
 }
