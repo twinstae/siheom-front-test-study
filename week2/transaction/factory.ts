d112227ed39aceb796b04c0abf67dc08a0208806
Taehee Kim <rabolution@gmail.com>
2026-01-09 21:26:10 +0900

    chore: library, format, lint

--- week2/transaction/factory.ts
+++ week2/transaction/factory.ts
@@ -1,5 +1,14 @@
+import invariant from '../invariant'
 import type { SimpleTransaction, ValidTransaction } from './type'
 
+const DATE_REGEX = /^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\d|3[01])$/i
+
 export function parseTransaction(transaction: SimpleTransaction): ValidTransaction {
+    invariant(DATE_REGEX.test(transaction.date), "[INVALID_DATE] " + transaction.date);
+    invariant(transaction.description.length < 1028, "[MAX_LENGTH] " + transaction.description);
+    invariant(DATE_REGEX.test(transaction.date), "[INVALID_DATE] " + transaction.date);
+    invariant(DATE_REGEX.test(transaction.date), "[INVALID_DATE] " + transaction.date);
+
+
     return transaction as ValidTransaction
 }
