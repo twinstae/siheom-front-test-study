d9e4484a69abf41542782ca321851d4ec50d962a
Taehee Kim <rabolution@gmail.com>
2026-01-09 22:11:13 +0900

    feat: schema 에러에 대한 테스트 하나 추가

--- week2/transaction/parser.test.ts
+++ week2/transaction/parser.test.ts
@@ -2,6 +2,16 @@ import { describe, it, expect } from "bun:test";
 import { notBalancedTransaction, validTransaction } from "./fixtures";
 import { parseTransaction } from "./parser";
 import { Temporal } from "temporal-polyfill";
+import * as v from 'valibot';
+import invariant from "../invariant";
+
+function safeTry<T>(fn: () => T): { result: T, error: undefined } | { result: undefined, error: unknown } {
+  try {
+    return { result: fn(), error: undefined };
+  } catch (error) {
+    return { result: undefined, error };
+  }
+}
 
 describe("parseTransaction", () => {
   it("balance가 0이 맞는 경우", async () => {
@@ -31,5 +41,18 @@ describe("parseTransaction", () => {
 
   it("balance가 0이 안 맞는 경우", async () => {
     expect(() => parseTransaction(notBalancedTransaction)).toThrow("Transaction is not balanced");
+  });
+
+  it("date가 올바르지 않은 경우", async () => {
+    const { error } = safeTry(() => parseTransaction({
+      ...validTransaction,
+      date: "2025-11-11 12:00:00",
+    }));
+
+    invariant(v.isValiError(error));
+    expect(v.summarize(error.issues)).toMatchInlineSnapshot(`
+      "× Invalid format: Expected /^\\d{4}-(?:0[1-9]|1[0-2])-(?:[12]\\d|0[1-9]|3[01])$/u but received "2025-11-11 12:00:00"
+        → at date"
+    `);
   });
 });
