e1df280c24b09e742ddf30b41a6c12ea17c1977e
Taehee Kim <rabolution@gmail.com>
2026-01-22 21:01:29 +0900

    feat: 1단계 과제

--- src/week4/form/NewTransactionForm.test.tsx
+++ src/week4/form/NewTransactionForm.test.tsx
@@ -27,61 +27,70 @@ function fillDate(label: string, date: Temporal.PlainDate) {
 }
 
 describe("NewTransactionForm", () => {
-  it("새 거래를 추가할 수 있다", async () => {
-    let result: SimpleTransaction | undefined;
+it("새 거래를 추가할 수 있다", async () => {
+  let result: SimpleTransaction | undefined;
 
-    await runSiheom(
-      given.render(
-        <NewTransactionForm
-          {...Stories.Empty.args}
-          addTransaction={async (transaction) => {
-            result = toSimpleTransaction(transaction);
-          }}
-        />,
-      ),
+  await runSiheom(
+    given.render(
+      <NewTransactionForm
+        {...Stories.Empty.args}
+        addTransaction={async (transaction) => {
+          result = toSimpleTransaction(transaction);
+        }}
+      />,
+    ),
 
-      fillDate("거래 일자", Temporal.PlainDate.from("2025-12-25")),
+    fillDate("거래 일자", Temporal.PlainDate.from("2025-12-25")),
 
-      actions.fill(query.textbox(/거래 설명/), "김토끼가 라즈베리파이를 구매함"),
+    // 거래 설명 텍스트박스에 "김토끼가 라즈베리파이를 구매함" 입력하기
 
-      actions.click(query.button("새 계정과목 추가하기")),
+    // 새 계정과목 추가하기 버튼을 클릭하기
 
-      actions.select(query.combobox(/계정과목 1/), "자산:유동자산:당좌자산:현금및현금성자산:현금"),
-      actions.fill(query.textbox(/금액 1/), "-120000"),
-      actions.select(query.button(/통화 1/), "KRW"),
+    // 계정과목 1 콤보박스에 현금 입력하기
 
-      actions.click(query.button("새 계정과목 추가하기")),
-      actions.click(query.button("새 계정과목 추가하기")),
+    // "자산:유동자산:당좌자산:현금및현금성자산:현금" 옵션을 선택하기
 
-      actions.click(query.button("계정과목 3 삭제하기")),
+    // 금액 1 텍스트박스에 -120000 입력하기
 
-      actions.select(query.combobox(/계정과목 2/), "자산:비유동자산:유형자산:기계장치"),
-      actions.fill(query.textbox(/금액 2/), "120000"),
-      
-      actions.select(query.combobox(/태그/), "구매"),
 
-      actions.click(query.button("거래 추가하기")),
+    // 통화 1 버튼을 클릭하고 KRW 옵션을 선택하기
 
-      assertions.a11ySnapshot(
-        query.form("새 거래 추가하기"),
-        "__snapshot__/new-transaction-form-with-data.snap",
-      ),
-    );
 
-    expect(result).toStrictEqual({
-      date: "2025-12-25",
-      description: "김토끼가 라즈베리파이를 구매함",
-      postings: [
-        {
-          account: "자산:유동자산:당좌자산:현금및현금성자산:현금",
-          amount: -120000,
-          commodity: "KRW",
-        },
-        { account: "자산:비유동자산:유형자산:기계장치", amount: 120000, commodity: "KRW" },
-      ],
-      tags: ["구매"],
-    });
+    actions.click(query.button("새 계정과목 추가하기")),
+    actions.click(query.button("새 계정과목 추가하기")),
+
+    actions.click(query.button("계정과목 3 삭제하기")),
+
+    actions.fill(query.combobox(/계정과목 2/), "기계장치"),
+    actions.click(query.option("자산:비유동자산:유형자산:기계장치")),
+    actions.fill(query.textbox(/금액 2/), "120000"),
+    
+    actions.fill(query.combobox(/태그/), "구매"),
+    actions.click(query.option("구매")),
+    actions.type(query.combobox(/태그/), "{Escape}"),
+
+    actions.click(query.button("거래 추가하기")),
+
+    assertions.a11ySnapshot(
+      query.form("새 거래 추가하기"),
+      "__snapshot__/new-transaction-form-with-data.snap",
+    ),
+  );
+
+  expect(result).toStrictEqual({
+    date: "2025-12-25",
+    description: "김토끼가 라즈베리파이를 구매함",
+    postings: [
+      {
+        account: "자산:유동자산:당좌자산:현금및현금성자산:현금",
+        amount: -120000,
+        commodity: "KRW",
+      },
+      { account: "자산:비유동자산:유형자산:기계장치", amount: 120000, commodity: "KRW" },
+    ],
+    tags: ["구매"],
   });
+});
 
   it("아무 값도 입력하지 않고 거래를 추가할 수 없다", async () => {
     let result: SimpleTransaction | undefined;
