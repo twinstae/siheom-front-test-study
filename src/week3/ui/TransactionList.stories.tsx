57b34333e9804b25c39328d287e47aa0cbcaf30b
Taehee Kim <rabolution@gmail.com>
2026-01-22 15:54:38 +0900

    fix: TransactionList 의 불필요한 주석을 제거한다

--- src/week3/ui/TransactionList.stories.tsx
+++ src/week3/ui/TransactionList.stories.tsx
@@ -1,11 +1,9 @@
-// Replace your-framework with the framework you are using, e.g. react-vite, nextjs, nextjs-vite, etc.
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { TransactionList } from "./TransactionList";
 import { parseTransaction } from "../../week2/transaction/parser";
 import { TestSimpleTransactionList } from "../../week2/transaction/fixtures";
 
-//👇 This default export determines where your story goes in the story list
 const meta = {
   component: TransactionList,
 } satisfies Meta<typeof TransactionList>;
